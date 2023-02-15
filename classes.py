f63721ffb3022f88f7d08ef8ab5ba7e723d739bd
Xinyi Julia Hoi <x.hoi@mysynergis.com>
2023-02-15 13:44:50 +0100

    Add samples and exercises

--- classes.py
+++ classes.py
@@ -1,8 +1,14 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+from abc import ABC, abstractmethod
+from collections import namedtuple
+
 # Classes => blueprint for creating new objects
 # Objects => instance of a class
 
 # custom classes
-
 class Point:
     # class level attributes => same attributes across all instances of a class
     default_color = "red"
@@ -52,10 +58,130 @@ class TagCloud:
     def __init__(self):
         self.tags = {}
 
-    def add(self, tag):
+    def add(self, tag): # add object
+        # check if object exists, if so 0 and if not +1
         self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
 
+    def __getitem__(self, tag): # get object
+        return self.tags.get(tag.lower(), 0)
+
+    def __setitem__(self, tag, count): # set object
+        self.tags[tag.lower()] = count
+
+    def __len__(self): # get length
+        return len(self.tags)
+
+    def __iter__(self): # makes class iterable
+        return iter(self.tags)
 
 cloud = TagCloud()
 cloud.add("python")
-print(cloud.tags)
+cloud["python"] = 10
+len(cloud.tags)
+
+# to make private members => __tags
+# to access => print(cloud._TagCloud__tags)
+
+# setting properties
+class Product:
+    def __init__(self, price):
+        self.price = price
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter # without setter => read only 
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Value cannot be less than 0.")
+        self.__price = value
+
+# product = Product(-1) # throws error
+
+
+# inheritance
+# multi-level inheritance => bad practice, limit to 1-2
+# multiple inheritance possible
+class Animal: # parent/ base class
+    def __init__(self):
+        self.color = "white"
+
+    def eat(self):
+        print("eat")
+
+
+class Dog(Animal): # child/ subclass
+    def __init__(self):
+        super().__init__() # super => access to base class, prevent method overwriting
+        self.size = "smol"
+
+
+    def walk(self):
+        print("walk")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("swim")
+
+
+dog = Dog()
+fish = Fish()
+dog.eat() # interited method
+print(fish.color) # inherited attribute 
+
+# object class
+# every class in python is derived from the object class
+# isinstance() => check if object is an instance of a class
+# issubclass() => check if class is a subclass of a class
+
+print(dog.color)
+print(dog.size)
+
+class InvalidOperationError(Exception): # custom exception
+    pass
+
+class Stream(ABC):
+    def __init__(self):
+        self.opened = False
+
+    def open(self):
+        if self.opened:
+            raise InvalidOperationError("Stream is open.")
+        self.opened = True
+
+    def close(self):
+        if not self.opened:
+            raise IndentationError("Stream is closed.")
+        self.opened = False
+
+    @abstractmethod # define common interface across all subclasses
+    def read(self): # if subclass does not have abstract method => subclass becomes abstract
+        pass
+
+class FileStream(Stream):
+    def read(self):
+        pass
+
+class NetworkStream(Stream):
+    def read(self):
+        pass
+
+
+# extending built-in types
+class Text(str): # built-in class
+    def duplicate(self):
+        return self + self
+
+class TrackableList(list):
+    def append(self, object): # overwriting append()
+        super().append(object)
+        print("Append called.")
+
+
+# when dealing with classes with only data and no methods
+# => namedtuple // can get attribute but not mutable
+Point1 = namedtuple("Point", ["x", "y"])
+p1 = Point1(x=1, y=2)
+p2 = Point1(x=1, y=2)
---
+++ modules.py
@@ -0,0 +1,21 @@
+from functions import greet
+import functions
+
+greet("Max", "Mustermann")
+functions.increment(10, 2)
+
+# if file is in subdirectory =>
+# add file named __init__.py (makes directory a package)
+# import =>
+# from dir_name import functions > functions.items()
+# import dir_name.functions > dir_name.functions.items()
+# from dir_name.functions import item1, item2 > items()
+
+# if file is in subpackage =>
+# add file named __init__.py
+# from dir_name1.dir_name2 import functions > functions.items()
+
+# absolute import => from dir_name1.dir#_name2 import functions
+# relative import => from ..dir_name2 import functions
+
+# dir() => get all attributes and methods in an object
